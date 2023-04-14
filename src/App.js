f67efb08ae5593b6ee9106f80d0e9aa8c40c6ab4
zmhp <manashahp1995@gmail.com>
2023-04-15 02:44:39 +0530

    Add header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'materialize-css/dist/css/materialize.min.css'
 // import M from 'materialize-css'
 
+import HeaderComponent from './HeaderComponent'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <HeaderComponent />
       </header>
     </div>
   );

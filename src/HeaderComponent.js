5af2bb1ddf64e87d4d51063a18651dc8d70637ec
zmhp <manashahp1995@gmail.com>
2023-04-15 02:51:24 +0530

    Add Header Component File

---
+++ src/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <header className="header">
+        Groccery List Manager
+    </header>
+  )
+}
+
+export default HeaderComponent
